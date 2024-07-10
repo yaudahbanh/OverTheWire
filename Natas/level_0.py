facde6a969d9e461fa4d8c9df6272ead1167dea1
yaudahbanh <juniorvincent32@gmail.com>
2024-07-10 17:54:26 +0700

    feat: add level_0

---
+++ Natas/level_0.py
@@ -0,0 +1,13 @@
+import requests
+import re
+
+url = "http://natas0.natas.labs.overthewire.org"
+
+def exp():
+    r = requests.get(url, auth=('natas0', 'natas0'))
+    if r.status_code == 200:
+        password = re.findall('<!--The password for natas1 is (.*) -->', r.text)[0]
+        print("Password for natas1: " + password)
+
+if __name__ == "__main__":
+    exp()
